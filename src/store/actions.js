42f09f0e31a4e36e07d8ef1ec764c60fe6747b5e
Anurag Sharma <asharma_tu@outlook.com>
2021-11-20 09:13:58 +0530

    Added Deposits File

--- src/store/actions.js
+++ src/store/actions.js
@@ -63,3 +63,86 @@ export function orderCancelled(order) {
     order
   }
 }
+
+// Fill Order
+export function orderFilling() {
+  return {
+    type: 'ORDER_FILLING'
+  }
+}
+
+export function orderFilled(order) {
+  return {
+    type: 'ORDER_FILLED',
+    order
+  }
+}
+
+// Balances
+export function etherBalanceLoaded(balance) {
+  return {
+    type: 'ETHER_BALANCE_LOADED',
+    balance
+  }
+}
+
+export function tokenBalanceLoaded(balance) {
+  return {
+    type: 'TOKEN_BALANCE_LOADED',
+    balance
+  }
+}
+
+export function exchangeEtherBalanceLoaded(balance) {
+  return {
+    type: 'EXCHANGE_ETHER_BALANCE_LOADED',
+    balance
+  }
+}
+
+export function exchangeTokenBalanceLoaded(balance) {
+  return {
+    type: 'EXCHANGE_TOKEN_BALANCE_LOADED',
+    balance
+  }
+}
+
+export function balancesLoaded() {
+  return {
+    type: 'BALANCES_LOADED'
+  }
+}
+
+export function balancesLoading() {
+  return {
+    type: 'BALANCES_LOADING'
+  }
+}
+
+export function etherDepositAmountChanged(amount) {
+  return {
+    type: 'ETHER_DEPOSIT_AMOUNT_CHANGED',
+    amount
+  }
+}
+
+export function etherWithdrawAmountChanged(amount) {
+  return {
+    type: 'ETHER_WITHDRAW_AMOUNT_CHANGED',
+    amount
+  }
+}
+
+export function tokenDepositAmountChanged(amount) {
+  return {
+    type: 'TOKEN_DEPOSIT_AMOUNT_CHANGED',
+    amount
+  }
+}
+
+export function tokenWithdrawAmountChanged(amount) {
+  return {
+    type: 'TOKEN_WITHDRAW_AMOUNT_CHANGED',
+    amount
+  }
+}
